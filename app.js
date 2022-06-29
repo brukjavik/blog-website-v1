0fadfb48e88f31638947ae2cb98b801837c4262c
brunoricky <bruninhocosta94@gmail.com>
2022-06-29 14:20:55 -0300

    changes update heroku

--- app.js
+++ app.js
@@ -56,6 +56,6 @@ app.post("/compose", function (req, res) {
   res.redirect("/");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("The server is up and running on port 3000");
 });
